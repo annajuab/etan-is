955af1d15fabe23558d5843ecf53c32bb3bb5e40
annajuab <138122674+annajuab@users.noreply.github.com>
2023-10-18 11:09:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function calcular(){
+    var valorEtanol = parseFloat (etanol.value);
+    var valorGasolina = parseFloat (gasolina.value);
+    
+    var custo = (valorEtanol/valorGasolina);
+
+if (custo > 0.7){
+    alert("Use gasolina")
+}
+if (custo < 0.7){
+    alert("Use álcool")
+}
+if (custo == 0.7){
+    alert("Tanto faz")
+}
+ }
